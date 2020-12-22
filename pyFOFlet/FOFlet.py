faaad5a2503d0b87029523d3c2628d27c98dec0a
Stuart Wichman <stuart.j@wichman-web.org>
2020-12-21 22:40:27 -0700

    pops and clicks, but everything works

--- pyFOFlet/FOFlet.py
+++ pyFOFlet/FOFlet.py
@@ -24,28 +24,31 @@ class FOFlet:
         self.FOFlet = []
 
     # functions to set values individually
-    def set_beta(self, beta_val):
-        self.beta = beta_val
-    def set_alpha(self, alpha_val):
-        self.alpha = alpha_val
+    def set_beta(self):
+        self.beta = np.pi/self.k1
+    def set_alpha(self):
+        self.alpha = self.BW*np.pi
     def set_BW(self, BW_val):
         self.BW = BW_val
+        self.set_alpha()
     def set_k1(self, k1_val):
         self.k1 = k1_val
+        self.set_beta()
     def set_Fc(self, Fc_val):
         self.Fc = Fc_val
     def set_Fs(self, Fs_val):
         self.Fs = Fs_val
+        self.Ts = 1/Fs_val
 
     # Generate new FOFlet from each impulse time.
     def update_FOFlet(self, Fs, impulse):
         tmp = []
-        for i in range(0,int(self.k1)):
-            attack_env = .5 * ( 1 - np.cos(self.beta*i) )
+        for i in range(0,int(self.k1*self.Fs)):
+            attack_env = .5 * ( 1 - np.cos(self.beta*i*self.Ts) )
             exponential = np.exp((-self.BW*np.pi*i)/self.Fs)
             msg = np.sin((2*np.pi*self.Fc*i)/Fs)
             tmp.append( attack_env*exponential*msg )
-        for i in range(int(self.k1)+1, int(Fs/impulse)):
+        for i in range(int(self.k1*self.Fs)+1, int(Fs/impulse)+1):
             decay_env = np.exp((-self.BW*np.pi*i)/self.Fs)
             msg = np.sin((2*np.pi*self.Fc*i)/Fs)
             tmp.append(decay_env * msg)
